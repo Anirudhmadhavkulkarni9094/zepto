cd6a4405fd063fc0da96620de0ce20d1ee190b7f
Anirudhmadhavkulkarni9094 <Anirudhkulkarni9094@gmail.com>
2024-01-16 00:13:27 +0530

    added spacing in suggestion

--- src/Component/Suggestion.js
+++ src/Component/Suggestion.js
@@ -21,7 +21,7 @@ function Suggestion({ users, name, onSelect, setShowSuggestion }) {
     </div> : <>
     <div className='absolute bg-white p-4 border-2 rounded-lg max-h-60 overflow-scroll w-fit'>
       { users.map(user => (
-          <div key={user.id} className="flex gap-4 hover:bg-gray-300 p-2 rounded-lg w-fit" onClick={() => {onSelect(user); setShowSuggestion(false)}}>
+          <div key={user.id} className="flex gap-4 hover:bg-gray-300 p-2 rounded-lg w-fit justify-between" onClick={() => {onSelect(user); setShowSuggestion(false)}}>
             <img src={require(`./Asset/${user.img}.png`)} className='w-6 h-6' alt=''></img>
             <h1>{user.name}</h1>
             <h1 className='text-gray-500'>{user.email}</h1>
